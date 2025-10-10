62509f077c1606ac68a673789064cacbef80e1f8
정서영 <diyung530@gmail.com>
2025-10-10 21:26:54 +0900

    Reconstruct test pages structure

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,19 +1,43 @@
-import PoseDetector from "@/components/pose-detector/PoseDetector";
+import DetectorCard from "./components/DetectorCard";
+import WebcamDetector from "@/components/detector/webcam-detector/WebcamDetector";
+import ImageDetector from "@/components/detector/image-detector/ImageDetector";
+import VideoDetector from "@/components/detector/video-detector/VideoDetector";
 
-export default function TestPage() {
+export default function DetectorTestPage() {
   return (
     <main className='min-h-screen bg-gradient-to-br from-blue-50 to-purple-50 p-6'>
-      <div className='container mx-auto max-w-5xl my-6'>
-        <div className='mb-6'>
-          <h1 className='text-3xl font-bold mb-2'>
-            Samadhi Pose Detection Test
+      <div className='container mx-auto my-10'>
+        <div className='mb-12 text-center'>
+          <h1 className='text-4xl font-extrabold mb-3 text-gray-800'>
+            포즈 감지 테스트
           </h1>
-          <p className='text-gray-600'>
-            MediaPipe 포즈 감지 및 관절 각도 계산 테스트
-          </p>
         </div>
 
-        <PoseDetector />
+        <div className='flex flex-wrap lg:flex-nowrap gap-6 items-stretch'>
+          <DetectorCard
+            title='웹캠'
+            linkHref='/test/webcam'
+            linkText='웹캠 테스트'
+          >
+            <WebcamDetector />
+          </DetectorCard>
+
+          <DetectorCard
+            title='이미지'
+            linkHref='/test/image'
+            linkText='이미지 테스트'
+          >
+            <ImageDetector />
+          </DetectorCard>
+
+          <DetectorCard
+            title='비디오'
+            linkHref='/test/video'
+            linkText='비디오 테스트'
+          >
+            <VideoDetector />
+          </DetectorCard>
+        </div>
       </div>
     </main>
   );
